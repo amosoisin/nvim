4113fad67e747d84747747d62de9c77e7ede02cd
amosoisin <szbh.itoshiki.izaya@gmail.com>
2021-02-12 20:27:59 +0900

    Do not to install neovim manually

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo apt -y install python3 python3-venv python3-pip clang libclang-dev llvm npm ctags clang-tools
 
-wget https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
-chmod +x nvim.appimage
-sudo mv nvim.appimage /usr/local/bin/nvim
+#wget https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
+#chmod +x nvim.appimage
+#sudo mv nvim.appimage /usr/local/bin/nvim
 
 sudo npm install -g neovim
 
